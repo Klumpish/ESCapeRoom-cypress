cf63ef14601f76d28ed3507622c9e43efe2b4bf3
Jörgen Lindström <jorgen.lindstrom@learniastudent.se>
2024-11-27 12:01:23 +0100

    Fixed styingd om cards

--- js/booking.js
+++ js/booking.js
@@ -16,134 +16,132 @@ const challengeInput = multiStepForm.querySelector("#challenge");
 const header1 = multiStepForm.querySelector("#header1");
 const header2 = multiStepForm.querySelector("#header2");
 
-
-import {ApiArray as events} from "../main.js";
-
+import { ApiArray as events } from "../main.js";
 
 openModal.addEventListener("click", () => {
-	modal.showModal();
-	const eventId = parseInt(openModal.getAttribute('data-id'));
-	const eventDetails = events.find(event => event.id === eventId);
-	header1.textContent = `Book room "${eventDetails.title}" (step 1)`;
-	
-	if (challengeInput) {
-		challengeInput.value = eventId;
-	} else {
-		console.error("The inputfield could not be found");
-	}
+  modal.showModal();
+  const eventId = parseInt(openModal.getAttribute("data-id"));
+  const eventDetails = events.find((event) => event.id === eventId);
+  header1.textContent = `Book room "${eventDetails.title}" (step 1)`;
+
+  if (challengeInput) {
+    challengeInput.value = eventId;
+  } else {
+    console.error("The inputfield could not be found");
+  }
 });
 
 closeModal.addEventListener("click", () => {
-	modal.close();
+  modal.close();
 });
 
 nextButton.addEventListener("click", () => {
-	const eventId = parseInt(openModal.getAttribute('data-id'));
-	const eventDetails = events.find(event => event.id === eventId);
-	header2.textContent = `Book room "${eventDetails.title}" (step 2)`;
-	const date = dateInput.value;
-	fetchAvailableTimes(date, 3);
+  const eventId = parseInt(openModal.getAttribute("data-id"));
+  const eventDetails = events.find((event) => event.id === eventId);
+  header2.textContent = `Book room "${eventDetails.title}" (step 2)`;
+  const date = dateInput.value;
+  fetchAvailableTimes(date, 3);
 
-	// Fyll select med deltagarantal
-	participantsSelect.innerHTML = ''; // Rensa tidigare alternativ
-	for (let i = 1; i <= eventDetails.maxParticipants; i++) {
-		const option = document.createElement('option');
-		option.value = i;
-		option.textContent = i;
-		participantsSelect.appendChild(option);
-	}
+  // Fyll select med deltagarantal
+  participantsSelect.innerHTML = ""; // Rensa tidigare alternativ
+  for (let i = 1; i <= eventDetails.maxParticipants; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = i;
+    participantsSelect.appendChild(option);
+  }
 });
 
 const simple = new Datepicker("#simple");
 
 let currentStep = formSteps.findIndex((step) => {
-	return step.classList.contains("active");
+  return step.classList.contains("active");
 });
 
 if (currentStep < 0) {
-	currentStep = 0;
-	showCurrentStep();
+  currentStep = 0;
+  showCurrentStep();
 }
 
 multiStepForm.addEventListener("click", (e) => {
-	if (e.target.matches("[next-data]")) {
-		currentStep += 1;
-	} else if (e.target.matches("[data-previous]")) {
-		currentStep -= 1;
-	}
-	showCurrentStep();
+  if (e.target.matches("[next-data]")) {
+    currentStep += 1;
+  } else if (e.target.matches("[data-previous]")) {
+    currentStep -= 1;
+  }
+  showCurrentStep();
 });
 
 function showCurrentStep() {
-	formSteps.forEach((step, index) => {
-		step.classList.toggle("active", index === currentStep);
-	});
+  formSteps.forEach((step, index) => {
+    step.classList.toggle("active", index === currentStep);
+  });
 }
 
 ////////////////////////////// FETCH AVAILABLE TIMESLOTS ///////////////////////
 
 async function fetchAvailableTimes(date, challenge) {
-	try {
-		const res = await fetch(
-			`https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=${date}&challenge=${challenge}` //date=${date}&challenge=${challenge}
-		);
-		if (!res.ok) {
-			throw new Error(`HTTP error! Status: ${res.status}`);
-		}
+  try {
+    const res = await fetch(
+      `https://lernia-sjj-assignments.vercel.app/api/booking/available-times?date=${date}&challenge=${challenge}` //date=${date}&challenge=${challenge}
+    );
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
 
-		const data = await res.json();
+    const data = await res.json();
 
-		if (Array.isArray(data.slots)) {
-			data.slots.forEach((slot) => {
-				const option = document.createElement("option");
-				option.value = slot; // Sets the value for <option>
-				option.textContent = slot; // Sets the text displayed in <option>
-				timeSelect.appendChild(option); // Adds <option> in <select>
-			});
-		} else {
-			console.log("No slots are available or slots is not an array");
-		}
-	} catch (error) {
-		console.error("Error fetching available times", error);
-	}
+    if (Array.isArray(data.slots)) {
+      data.slots.forEach((slot) => {
+        const option = document.createElement("option");
+        option.value = slot; // Sets the value for <option>
+        option.textContent = slot; // Sets the text displayed in <option>
+        timeSelect.appendChild(option); // Adds <option> in <select>
+      });
+    } else {
+      console.log("No slots are available or slots is not an array");
+    }
+  } catch (error) {
+    console.error("Error fetching available times", error);
+  }
 }
 
 /////////////////////////////////  SUBMIT FORM ////////////////////////////////
 
 multiStepForm.addEventListener("submit", (e) => {
-	e.preventDefault(); // prevent form submission
+  e.preventDefault(); // prevent form submission
 
-	const formData = new FormData(multiStepForm);
+  const formData = new FormData(multiStepForm);
 
-	// send the data somewhere
-	console.log(Object.fromEntries(formData));
+  // send the data somewhere
+  console.log(Object.fromEntries(formData));
 
-	//////////////////////////////// POST DATA TO API /////////////////////////
-	// async function sendBooking(){
-	//   const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/booking/reservations', {
-	//     method: 'POST',
-	//     headers: {
-	//         'Content-Type': 'application/json',
-	//     },
-	//     body: JSON.stringify({
-	//         challenge: 12,
-	//         name: "Customer Name",
-	//         email: "name@example.com",
-	//         date: "2024-12-12",
-	//         time: "18:30",
-	//         participants: 4,
-	//     }),
-	// });
-	// const data = await res.json();
-	// console.log(data);
-	// }
+  //////////////////////////////// POST DATA TO API /////////////////////////
+  // async function sendBooking(){
+  //   const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/booking/reservations', {
+  //     method: 'POST',
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //         challenge: 12,
+  //         name: "Customer Name",
+  //         email: "name@example.com",
+  //         date: "2024-12-12",
+  //         time: "18:30",
+  //         participants: 4,
+  //     }),
+  // });
+  // const data = await res.json();
+  // console.log(data);
+  // }
 
-	/////////////////////////////////// SHOW COMPLETESIGN //////////////////////////////
-	submitButton.disabled = true;
-	submitButton.textContent = "Submitting...";
+  /////////////////////////////////// SHOW COMPLETESIGN //////////////////////////////
+  submitButton.disabled = true;
+  submitButton.textContent = "Submitting...";
 
-	// mimic a server request
-	setTimeout(() => {
-		multiStepForm.querySelector(".multi-step-form__completed").hidden = false;
-	}, 3000);
+  // mimic a server request
+  setTimeout(() => {
+    multiStepForm.querySelector(".multi-step-form__completed").hidden = false;
+  }, 3000);
 });
