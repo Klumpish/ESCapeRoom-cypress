1c11c56755c60d749f4e9beec8459f8693acbe44
Henrik <Henrikmattsson89@gmail.com>
2024-11-27 13:24:19 +0100

    changed createChallenge if statment

--- challenges.js
+++ challenges.js
@@ -96,7 +96,7 @@ const challengeSort = {
 	 * Function to create a challenge card.
 	 */
 	createChallenge() {
-		if (this.currentPath === "/" || this.currentPath.includes("/index.html")) {
+		if (this.currentPath != "/challenges.html" || this.currentPath.includes("/index.html")) {
 			const cardContainer = document.querySelector(".card__container");
 
 			/* loop through the three highest rated */
