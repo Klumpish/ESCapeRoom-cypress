0c3df8fd09af245121d17b0bc826fb2fa16969e1
Henrik <Henrikmattsson89@gmail.com>
2024-11-28 13:58:56 +0100

    fixed our post request and setup docs for our testing

--- __tests__/jest.test.js
+++ __tests__/jest.test.js
@@ -1,40 +1,39 @@
-import fetchMock from 'jest-fetch-mock'; 
-import {getApiToArray} from '../challenges.js'; 
+import fetchMock from "jest-fetch-mock";
+import { getApiToArray } from "../challenges.js";
 
-fetchMock.enableMocks(); 
+fetchMock.enableMocks();
 
-beforeEach(() => { 
-    fetch.resetMocks(); 
+beforeEach(() => {
+	fetch.resetMocks();
 });
 
-test('getApiToArray makes API call and returns data', async () => {
-   fetch.mockResponseOnce(JSON.stringify({ challenges: []}));
+test("getApiToArray makes API call and returns data", async () => {
+	fetch.mockResponseOnce(JSON.stringify({ challenges: [] }));
 
-   const data = await getApiToArray();
+	const data = await getApiToArray();
 
-   expect(fetch).toHaveBeenCalledWith('https://lernia-sjj-assignments.vercel.app/api/challenges');
+	expect(fetch).toHaveBeenCalledWith("https://lernia-sjj-assignments.vercel.app/api/challenges");
 
-   expect(data).toEqual([]);
-    });
+	expect(data).toEqual([]);
+});
 
-    test('getApiToArray returns an empty array when API call fails', async () => { 
-        fetch.mockReject(() => Promise.reject('API is down'));
-        
-        const data = await getApiToArray();
+test("getApiToArray returns an empty array when API call fails", async () => {
+	fetch.mockReject(() => Promise.reject("API is down"));
 
-        expect(fetch).toHaveBeenCalledWith('https://lernia-sjj-assignments.vercel.app/api/challenges');
-        // Empty array returns when error
-        expect(data).toEqual([]); 
-    
-    });
+	const data = await getApiToArray();
 
-    test('getApiToArray logs an error message when API call fails', async () => { 
-        const consoleSpy = jest.spyOn(console, 'error'); 
-        fetch.mockReject(() =>Promise.reject(new Error('API is down')));  //Added (new Error ) because that is an object and test failed when it was a string
+	expect(fetch).toHaveBeenCalledWith("https://lernia-sjj-assignments.vercel.app/api/challenges");
+	// Empty array returns when error
+	expect(data).toEqual([]);
+});
 
-        await getApiToArray();
+test("getApiToArray logs an error message when API call fails", async () => {
+	const consoleSpy = jest.spyOn(console, "error");
+	fetch.mockReject(() => Promise.reject(new Error("API is down"))); //Added (new Error ) because that is an object and test failed when it was a string
 
-        expect(consoleSpy).toHaveBeenCalledWith('Error in getApiToArray', expect.any(Error));
+	await getApiToArray();
 
-        consoleSpy.mockRestore();
-    });
+	expect(consoleSpy).toHaveBeenCalledWith("Error in getApiToArray", expect.any(Error));
+
+	consoleSpy.mockRestore();
+});
